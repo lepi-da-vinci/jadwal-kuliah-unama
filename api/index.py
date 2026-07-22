4a76951c483f307d3e7cdfa8c0f5f4a5fc4bacf1
lepi da vinci <muhamadrezaf00@gmail.com>
2026-07-22 17:33:23 +0700

    Fix: Tambah dukungan DB_PORT untuk Aiven

--- api/index.py
+++ api/index.py
@@ -23,6 +23,7 @@ app.add_middleware(
 def get_db():
     return mysql.connector.connect(
         host=os.environ.get("DB_HOST", "localhost"),
+        port=int(os.environ.get("DB_PORT", 3306)),
         user=os.environ.get("DB_USER", "root"),
         password=os.environ.get("DB_PASSWORD", ""),
         database=os.environ.get("DB_NAME", "db_jadwal_kuliah")
